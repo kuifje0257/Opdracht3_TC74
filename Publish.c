4146b50cd51dc6c578a1192f0d238daaeeb9858d
kuifje0257 <kuifje0257@hotmail.be>
2021-11-22 11:31:15 +0100

    Removed ph and made own converter

--- Publish.c
+++ Publish.c
@@ -2,7 +2,8 @@
 #include "stdlib.h"
 #include "string.h"
 #include "unistd.h"
-#include "math.h"
+#include <math.h>
+#include <assert.h>
 #include "MQTTClient.h"
 
 #define ADDRESS     "tcp://localhost:1883"
@@ -12,15 +13,15 @@
 #define QOS         1
 #define TIMEOUT     1000L
 
-int sysCommand(){
-   char command[50];
-   char hex[100];
-
-   strcpy(command, "sudo i2cget -y 1 0x48 0 b" );
-   system(command);
-   
-   return (0);
-} 
+size_t chopN(char *str, size_t n)
+{
+    assert(n != 0 && str != 0);
+    size_t len = strlen(str);
+    if (n > len)
+        n = len;
+    memmove(str, str+n, len - n + 1);
+    return(len - n);
+}
 
 
 int main(int argc, char* argv[])
@@ -43,26 +44,69 @@ int main(int argc, char* argv[])
         printf("Failed to connect, return code %d\n", rc);
         exit(-1);
     }
-    Sleep(2);
- 
-    int c = sysCommand();
-
-    //nergens kan ik het resultaat van system(...) opvangen om om te zetten naar decimaal
-
-    printf("...\n");
-    printf("...\n");
-    char result[50];
- 
-    sprintf(result,"%f", c);
-    pubmsg.payload = result;
-    printf("result = %s\n",result);
+    sleep(2);
     
-    
-    //testbuf omdat mqtt werkt
-    //sprintf(result,"hello");
-    //pubmsg.payload = result;
+    FILE *p;
+    char buf[50];
 
-    if(ph != result){
+    p = popen("sudo i2cget -y 1 0x48 0 b","r");
+
+    if( p == NULL)
+
+    {
+        puts("Unable to open process");
+        return(1);
+    }
+
+    fgets(buf,50,p);  
+    printf("temperature is %s",buf);
+
+    chopN(buf,2);
+
+    //
+
+    long long decimal, place;
+    int i = 0, val, len;
+
+    
+    decimal = 0;
+    place = 1;
+
+
+    /* Find the length of total number of hex digit */
+    len = strlen(buf);
+    len--;
+
+    /*
+     * Iterate over each hex digit
+     */
+    for(i=0; buf[i]!='\0'; i++)
+    {
+ 
+        /* Find the decimal representation of hex[i] */
+        if(buf[i]>='0' && buf[i]<='9')
+        {
+            val = buf[i] - 48;
+        }
+        else if(buf[i]>='a' && buf[i]<='f')
+        {
+            val = buf[i] - 97 + 10;
+        }
+        else if(buf[i]>='A' && buf[i]<='F')
+        {
+            val = buf[i] - 65 + 10;
+        }
+
+        decimal += val * pow(16, len);
+        len--;
+    }
+
+    printf("Hexadecimal number = %s\n", buf);
+    printf("Decimal number = %lld\n", decimal);
+    //
+    pubmsg.payload = buf;
+
+    if(ph != buf){
         pubmsg.payloadlen = 50;
         pubmsg.qos = QOS;
         pubmsg.retained = 0;
@@ -70,15 +114,16 @@ int main(int argc, char* argv[])
         MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token);
         printf("Waiting for up to %d seconds for publication of %s\n"
                 "on topic %s for client with ClientID: %s\n",
-                (int)(TIMEOUT/1000), result, TOPIC, CLIENTID);
+                (int)(TIMEOUT/1000), buf, TOPIC, CLIENTID);
         rc = MQTTClient_waitForCompletion(client, token, TIMEOUT);
         printf("Message with delivery token %d delivered\n", token);
         
         return rc;
-        sprintf(ph, result);
+        sprintf(ph, buf);
 
     }
     else{
-        print("no changes");
+        printf("no changes");
     }
+    pclose(p);
 }
